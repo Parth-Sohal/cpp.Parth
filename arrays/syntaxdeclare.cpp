9caea7b81ad41b04812eb3a7d1dfbf58733353d8
Parth Sohal <sohalparth72@gmail.com>
2025-01-26 10:54:39 +0530

    marks35

--- arrays/syntaxdeclare.cpp
+++ arrays/syntaxdeclare.cpp
@@ -8,6 +8,15 @@ int main(){
     // for(int i = 0 ;i < 9;i++){
     //     cout << arr[i];
     // }
-    int arr[] = {1,2,3,4,5,6,7};
+    // int arr[] = {1,2,3,4,5,6,7};
+    int arr[10];
+    for(int i = 0; i < 10 ;i++){
+        cin >> arr[i];
+    }
+    for(int i = 0 ; i < 10 ; i++){
+        if(arr[i]<35){
+            cout <<  i << endl;
+        }
+    }
     return 0;
 }
