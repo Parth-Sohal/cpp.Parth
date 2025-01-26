acf40c1e1caebb0da94948088719984a9a725d95
Parth Sohal <sohalparth72@gmail.com>
2025-01-26 10:46:26 +0530

    yes new

---
+++ arrays/syntaxdeclare.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int arr[10];
+    // for(int i = 0 ;i < 9;i++){
+    //     cin >> arr[i];
+    // }
+    // for(int i = 0 ;i < 9;i++){
+    //     cout << arr[i];
+    // }
+    int arr[] = {1,2,3,4,5,6,7};
+    return 0;
+}
