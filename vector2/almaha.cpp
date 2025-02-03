77a5a37861b75c8a387dd353659337580fb6a8ed
Parth Sohal <sohalparth72@gmail.com>
2025-02-03 23:35:10 +0530

    memory

---
+++ vector2/almaha.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // vector<int> v{1,2,3};
+    // int* ptr =  &v[0];
+    // for(int i = 0  ; i<v.size();i++){
+    //     cout << ptr << " ";
+    //     ptr++;
+    // }
+    // v.push_back(7);
+    // cout << endl;
+    // int* ptr1 =  &v[0];
+    // for(int i = 0  ; i<v.size();i++){
+    //     cout << ptr1 << " ";
+    //     ptr1++;
+    // }
+    return 0;
+}
