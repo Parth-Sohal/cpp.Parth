71c481ee0847b46bf9a7bb64b7f72bdcdb01d099
Parth-Sohal <sohalparth72@gmail.com>
2025-02-27 19:17:15 +0530

    hojayega

---
+++ leetcodequestion/pair/smallestmissingnumber.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <limits.h>
+using namespace std;
+
+int main(){
+    
+    // for(int i = 1 ; i < arr.size() ;i++){
+    //     bool flag = false;
+    //     for(int j = 0 ;j < arr.size() ;j++){
+    //         if(arr[j] == i){
+    //             flag = true;
+    //         }
+    //     }
+    //     if(!flag){
+    //         cout << i << endl;
+    //         break;
+    //     }
+    // }
+
+    // optimised 
+
+    // vector<int> arr = {6,5,0,4,3,2,1};
+    // int min = -1;
+
+    // for(int i = 0 ; i < arr.size() ;i++){
+    //     if(arr[i] == 1){
+    //         min = i;
+    //     }
+    // }
+    // if(min == -1)
+    // {
+    //     cout << 1;
+    // }    
+    // else{
+    //     int count = 1;
+    //     int sum = 1;
+    //     for(int i = 0 ;i < arr.size() ;i++){
+    //         if(arr[i]>1){
+    //             count++;
+    //             sum+=arr[i];
+    //         }
+    //     }
+    //     cout << count << endl;
+    //     cout << sum;
+    // }
+
+}
