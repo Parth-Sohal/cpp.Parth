9bcfaad4da1c8935413895f34a03b00d5e53bf03
Parth-Sohal <sohalparth72@gmail.com>
2025-03-07 16:13:23 +0530

    leetcode wla easy

--- 2poinetr/2sum.cpp
+++ 2poinetr/2sum.cpp
@@ -1,13 +1,14 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<limits.h>
-#include<cmath>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+
     // for(int i = 0 ; i < arr.size() ;i++){
     //     for(int j = i+1 ; j < arr.size()  ;j++){
     //         for(int k = j+1 ; j<arr.size() ;j++){
@@ -17,25 +18,46 @@ int main(){
     //         }
     //     }
     // }
-    vector<int> arr = {1, 3, 5, 8, 12};
-    int target = 25;
-    int low = 0 , mid = 1 , high = arr.size()-1;
-    vector<vector<int>> nums;
-    while(mid != high){
-        if(arr[low]+arr[mid]+arr[high] == target){
-            nums.push_back({arr[low],arr[mid],arr[high]});
-        }
-        if(arr[low]+arr[mid]+arr[high] > target)
-        {
-            high--;
+    // vector<int> arr = {1, 3, 5, 8, 12};
+    // int target = 25;
+    // int low = 0 , mid = 1 , high = arr.size()-1;
+    // vector<vector<int>> nums;
+    // while(mid != high){
+    //     if(arr[low]+arr[mid]+arr[high] == target){
+    //         nums.push_back({arr[low],arr[mid],arr[high]});
+    //     }
+    //     if(arr[low]+arr[mid]+arr[high] > target)
+    //     {
+    //         high--;
+    //     }
+    //     else{
+    //         low++,mid++;
+    //     }
+    // }
+    // for(int i = 0 ; i < nums.size() ;i++){
+    //     for(int j = 0 ; j < nums[i].size() ;j++){
+    //         cout << j << " ";
+    //     }
+    // }
+    vector<int> nums = {1,5,1,4,2};
+
+    int i = 0 ; 
+    int j = 0;
+    while(j < nums.size()){
+        int element = nums[j];
+        if(element % 2 == 0){
+            nums[j] = 0;
+            swap(nums[i],nums[j]);
+            i++;
+            j++;
         }
         else{
-            low++,mid++;
+            nums[j] = 1;
+            j++;
         }
     }
-    for(int i = 0 ; i < nums.size() ;i++){
-        for(int j = 0 ; j < nums[i].size() ;j++){
-            cout << j << " ";
-        }
+    for(auto i : nums){
+        cout << i << " ";
     }
+    
 }
